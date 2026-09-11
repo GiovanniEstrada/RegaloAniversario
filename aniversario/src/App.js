37e1db599eff6b42d0dcac15fed403b2d68821fe
GiovanniEstrada <cgiovanniestrada@gmail.com>
2026-09-10 23:00:02 -0600

    Quita useeffect

--- aniversario/src/App.js
+++ aniversario/src/App.js
@@ -61,7 +61,7 @@
 
 // export default App;
 
-import React, { useState, useEffect } from "react";
+import  { useState } from "react";
 import IntroPage from "./pages/IntroPage";
 import Home from "./pages/Home";
 
